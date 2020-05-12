b8093afa18920fd3b1992c2696f5964f29628c97
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-05-11 21:14:40 -0400

    chiron: Upver NFC hal to version 1.2
    Change-Id: I5a0a04c3d103d3f1292df95395e8f74138b1a18c
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- device.mk
+++ device.mk
@@ -207,7 +207,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.1-service \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     Tag
 
