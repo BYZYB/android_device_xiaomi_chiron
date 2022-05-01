ed1b498e32a179702db93dff73c4b6910fa2cb9d
Edwin Wong <edwinwong@google.com>
2022-05-01 19:05:33 +0800

    chiron: clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I106a727f2f29bf5472484402b2801a630b0d6301

--- device.mk
+++ device.mk
@@ -105,7 +105,7 @@ PRODUCT_PACKAGES += libshim_dpmframework
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8998
