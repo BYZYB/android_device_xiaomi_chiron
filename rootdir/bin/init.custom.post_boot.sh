3807072b07753ee466ec062d649bc98c44a5d53d
BYZYB <zyb_1998@outlook.com>
2023-01-10 21:45:28 +0800

    chiron: Update repo diff (20230110)
    Update the patch about keeping SELinux permissive in user build variant.
    
    Change-Id: I01053196eb37761c2785ba8075ce6d7fff6158ce

--- rootdir/bin/init.custom.post_boot.sh
+++ rootdir/bin/init.custom.post_boot.sh
@@ -9,5 +9,5 @@ find /data/data/com.tencent.mm/MicroMsg/*/image2/* -mtime +7 -exec rm -rf {} \;
 find /data/data/com.tencent.mm/MicroMsg/*/video/* -mtime +7 -exec rm -rf {} \;
 find /data/data/com.tencent.mm/MicroMsg/*/voice2/* -mtime +7 -exec rm -rf {} \;
 find /data/media/0/Android/data/com.tencent.mm/MicroMsg/*/* -mtime +3 -exec rm -rf {} \;
-rm -rf /data/anr /data/bootchart /data/local/tests /data/local/traces /data/misc/a11ytrace /data/misc/boottrace /data/misc/snapshotctl_log /data/misc/trace /data/misc/update_engine /data/misc/update_engine_log /data/misc/wmtrace /data/system/dropbox /data/system/heapdump /data/tombstones /data/vendor/tombstones
+rm -rf /data/anr /data/bootchart /data/local/tests /data/local/traces /data/misc/a11ytrace /data/misc/boottrace /data/misc/snapshotctl_log /data/misc/trace /data/misc/update_engine /data/misc/update_engine_log /data/misc/wmtrace /data/system/dropbox /data/system/heapdump /data/system/uiderrors.txt /data/tombstones /data/vendor/tombstones
 rm -rf /data/backup/pending/* /data/data/*/cache/* /data/data/*/code_cache/* /data/local/tmp/* /data/media/0/*/.thumbnails/* /data/media/0/Android/data/*/cache/* /data/user_de/*/*/cache/* /data/user_de/*/*/code_cache/*
