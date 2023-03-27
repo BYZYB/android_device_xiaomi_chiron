7e6f576133f228cdc4876fe91cf37f28f453a097
Gabriel Biren <gbiren@google.com>
2023-03-27 22:33:41 +0000

    chiron: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- device.mk
+++ device.mk
@@ -363,7 +363,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     ipacm \
     IPACM_cfg.xml \
