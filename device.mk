a42c5002675b35297050e3dd44d11bcfc7910a71
me-cafebabe <me.cafebabe@gmail.com>
2023-10-07 17:26:01 +0800

    chiron: Switch to legacy WiFi HIDL HAL
    * With the new WiFi AIDL HAL, the framework thinks the device
      supports interface combination for some unknown reason,
      even though the service doesn't advertise it. As a result,
      the WiFi service fails on configureChip method.
    
    Change-Id: I0c3fa7b4c947c3525c30f7cef0b2674bee224da7
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- device.mk
+++ device.mk
@@ -363,7 +363,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi-service \
+    android.hardware.wifi@1.0-service.legacy \
     hostapd \
     ipacm \
     IPACM_cfg.xml \
