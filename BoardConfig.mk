f1d511a83c22ac5f0a989c44cf1bf164dfd2a2d8
BYZYB <zyb_1998@outlook.com>
2024-03-06 22:47:20 +0800

    chiron: recovery: Enable logcat by default
    Change-Id: Ib2e500a0cbd7440be760885de76b6efc7301b7d4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,5 +86,5 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
 
 # TWRP debug
-# TARGET_USES_LOGD := true
-# TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
