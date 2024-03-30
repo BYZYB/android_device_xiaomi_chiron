09cc6f8467f6f8fda8e851bfb6c5ef136bd3ad97
BYZYB <zyb_1998@outlook.com>
2024-03-30 13:30:47 +0800

    chiron: Don't explicitly include languages_full.mk
    It is inherited by full_base_telephony.mk -> aosp_base.mk -> full_base.mk .
    
    Change-Id: Ia212411cbf9eb3122d1950d9f8d11bab5190c407

--- lineage_chiron.mk
+++ lineage_chiron.mk
@@ -17,17 +17,16 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-$(call inherit-product, vendor/xiaomi/chiron/chiron-vendor.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/chiron/device.mk)
+$(call inherit-product, vendor/xiaomi/chiron/chiron-vendor.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Xiaomi
