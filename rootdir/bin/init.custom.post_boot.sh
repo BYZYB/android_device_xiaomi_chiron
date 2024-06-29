3164a7f7c19bdce2aec06fa9c68e7968c1cade47
BYZYB <zyb_1998@outlook.com>
2024-06-29 21:41:11 +0800

    chiron: Update repo diff (20240629)
    Adapt for platform update `AP2A.240605.024`.
    Always set `BUILD_KEYS := release-keys` to avoid root detection of some apps.
    Remove full weight notocjk fonts as "NotoSansCJK-VF.otf.ttc" will be shipped.
    
    Change-Id: Ida694f0e7411773f8a01a7b1731bbecda6fabebe

--- rootdir/bin/init.custom.post_boot.sh
+++ rootdir/bin/init.custom.post_boot.sh
@@ -14,5 +14,5 @@ if [ -f "/data/custom_cleanup" ]; then
     find /data/media/*/Android/data/com.tencent.mm/MicroMsg/*/* -mtime +3 -exec rm -rf {} \;
 fi
 
-rm -rf /data/anr /data/bootchart /data/local/tests /data/local/traces /data/misc/a11ytrace /data/misc/boottrace /data/misc/nfc/logs /data/misc/snapshotctl_log /data/misc/trace /data/misc/update_engine /data/misc/update_engine_log /data/misc/wmtrace /data/ota /data/ota_package /data/system/dropbox /data/system/graphicsstats /data/system/heapdump /data/system/procexitstore /data/system/procstats /data/system/shutdown-checkpoints /data/tombstones /data/vendor/tombstones
+rm -rf /data/anr /data/bootchart /data/local/tests /data/local/traces /data/misc/a11ytrace /data/misc/bluetooth/logs /data/misc/boottrace /data/misc/nfc/logs /data/misc/snapshotctl_log /data/misc/trace /data/misc/update_engine /data/misc/update_engine_log /data/misc/wmtrace /data/ota /data/ota_package /data/system/dropbox /data/system/graphicsstats /data/system/heapdump /data/system/procexitstore /data/system/procstats /data/system/shutdown-checkpoints /data/tombstones /data/vendor/tombstones
 rm -rf /data/backup/pending/* /data/data/*/cache/* /data/data/*/code_cache/* /data/local/tmp/* /data/media/*/*/.thumbnails/* /data/media/*/Android/data/*/cache/* /data/user_de/*/*/cache/* /data/user_de/*/*/code_cache/* /data/system/uiderrors.txt
