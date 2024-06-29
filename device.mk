977ef8b696843e1301e3a0230f1408caf522e321
Aaron Kling <webgeek1234@gmail.com>
2024-06-28 22:09:34 -0500

    chiron: Remove software omx codec references
    These are no longer available as of Android 14 QPR3. The C2 codecs
    available by default in the swcodec apex will be used instead.
    
    Change-Id: I8cdf3e700823779a7382c66c3df8410d41209b7c

--- device.mk
+++ device.mk
@@ -205,10 +205,7 @@ PRODUCT_PACKAGES += vendor.lineage.livedisplay@2.0-service-sdm
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+    $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # Mlipay
 PRODUCT_PACKAGES += \
