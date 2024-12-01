6524cc3feb4aae78978e1f084e0f6a5f401bbcca
Michael Bestas <mkbestas@lineageos.org>
2024-12-01 22:30:22 +0200

    chiron: gps: Convert remaining Android.mk to bp
    Change-Id: I75e6af92795fe755a48557aa06106406b14c6750

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,9 +57,6 @@ TARGET_USES_VULKAN := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
-# GPS
-BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
-
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
